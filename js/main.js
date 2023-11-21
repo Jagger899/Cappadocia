4a3952327a5e738b724696daac30d65cd711d62b
Roma Artiukh <romanskarb@ukr.net>
2023-11-21 17:38:37 +0300

    header, fairy-tail,hotel,trip

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const swiper1 = new Swiper('.fairy-tail__swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-right',
+    prevEl: '.swiper-button-left',
+  },
+
+});
+
+const swiper2 = new Swiper('.trip__swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-right',
+    prevEl: '.swiper-button-left',
+  },
+
+});
