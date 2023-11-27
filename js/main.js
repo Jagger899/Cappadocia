90c0062c74a2807c362072e7f605683debaa3aeb
Jagger899 <romanskarb@ukr.net>
2023-11-27 15:02:10 +0300

    Исправил ошибки согласно замечаниям

--- js/main.js
+++ js/main.js
@@ -26,3 +26,24 @@ const form = document.getElementById("form");
 form.addEventListener('click', function (event) {
   event.preventDefault();
 });
+
+const header = document.getElementById('header');
+console.log(header)
+const tour = document.getElementById('tour');
+
+window.addEventListener('scroll', function () {
+  console.log('scroll is run');
+  header.classList.add('header--sticky');
+  tour.classList.add('tour--with-margin');
+});
+
+window.addEventListener('scroll', function () {
+  const scrollPosition = window.scrollY;
+  console.log(scrollPosition);
+
+  if (scrollPosition === 0) {
+    header.classList.remove('header--sticky');
+    tour.classList.remove('tour--with-margin');
+  }
+
+});
