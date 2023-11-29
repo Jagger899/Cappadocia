476d2aeb73bb24acf101e5812724a9c7813fe684
Jagger899 <romanskarb@ukr.net>
2023-11-29 13:14:34 +0300

    Правки согл ком 2

--- js/main.js
+++ js/main.js
@@ -48,15 +48,12 @@ form.addEventListener('click', function (event) {
 });
 
 const header = document.getElementById('header');
-console.log(header)
+
 const tour = document.getElementById('tour');
 // const headerContainer = document.getElementsByClassName('header__container')[0];
 const headerContainer = document.getElementById('header__container')
-console.log(headerContainer);
-
 
 window.addEventListener('scroll', function () {
-  console.log('scroll is run');
   header.classList.add('header--sticky');
   tour.classList.add('tour--with-margin');
   // headerContainer.style = "padding-top: 50px";
@@ -65,13 +62,11 @@ window.addEventListener('scroll', function () {
 
 window.addEventListener('scroll', function () {
   const scrollPosition = window.scrollY;
-  console.log(scrollPosition);
   if (scrollPosition === 0) {
     header.classList.remove('header--sticky');
     tour.classList.remove('tour--with-margin');
     // headerContainer.style = "padding-top:74px";
     headerContainer.classList.remove('header__container--sticky');
-    
   }
 });
 
@@ -88,18 +83,17 @@ window.addEventListener('scroll', function () {
 // }
 
 
-// console.log(window.innerWidth)
+
 
 const mobileMenu = document.getElementById('mobile-menu');
 const mobileMenuOpen = document.getElementById('mobile-menu-open');
 const mobileMenuClose = document.getElementById('mobile-menu-close');
-console.log(mobileMenu)
 
 mobileMenuOpen.addEventListener('click', function () {
   mobileMenu.classList.add('mobile-menu--open');
 });
 
-mobileMenuClose.addEventListener('click', function () {
+mobileMenuClose.addEventListener('click', function (event) {
   mobileMenu.classList.remove('mobile-menu--open');
   event.preventDefault();
 })
